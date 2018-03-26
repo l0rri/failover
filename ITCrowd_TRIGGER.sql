522a24aaa051e394f434aad80ba0c32d69eda3ab
arink <arink@eecs.wsu.edu>
2018-03-25 21:58:58 -0700

    added trigger test fail case

--- ITCrowd_TRIGGER.sql
+++ ITCrowd_TRIGGER.sql
@@ -215,8 +215,9 @@ EXECUTE PROCEDURE updateCheckinCount();
 /* Test trigger statements */
 /*
 SET datestyle=ymd;
+/* Test a valid review insert */ 
 INSERT INTO yelp_business 
-	VALUES(1234567,'Tester', '1234 1st st', 'Pullman', 'WA', '99163', false, 0,0,0,0,0,0,0);
+	VALUES(1234567,'Tester', '1234 1st st', 'Pullman', 'WA', '99163', true, 0,0,0,0,0,0);
 INSERT INTO yelp_user(user_id)
 	VALUES (9876);
 INSERT INTO yelp_checkin (business_id,day_of_week, morning, afternoon, evening, night)
@@ -225,6 +226,18 @@ INSERT INTO yelp_review
 	VALUES (1234567, 54321, 9876, 5, '2018-03-22','Great experience!',1,1,1);
 INSERT INTO yelp_review
 	VALUES (1234567, 54322, 9876, 1, '2018-03-22','Meh :(',1,1,1);
+	
+/* Test an invalid insert - should fail is_open trigger */
+	INSERT INTO yelp_business 
+	VALUES('abcdefg','TestFail', '1234 1st st', 'Pullman', 'WA', '99163', false, 0,0,0,0,0,0);
+INSERT INTO yelp_user(user_id)
+	VALUES ('abcd');
+INSERT INTO yelp_checkin (business_id,day_of_week, morning, afternoon, evening, night)
+	VALUES ('abcdefg','Monday', 0,0,0,1);
+INSERT INTO yelp_review
+	VALUES ('abcdefg', 678987, 'abcd', 5, '2018-03-22','Great experience!',1,1,1);
+INSERT INTO yelp_review
+	VALUES ('abcdefg', 67898, 'abcd', 1, '2018-03-22','Meh :(',1,1,1);
 SELECT * FROM yelp_business;
 */
 /* Results: reviewcount=2,stars=6,reveiwrating=3.0,numcheckins=2 */
