30fd147514f5a049bd9634810d0ca124115ce686
= <arink@eecs.wsu.edu>
2018-04-24 11:50:50 -0700

    change checkin text when business is selected

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -629,6 +629,9 @@ namespace WindowsFormsApp1
             var b4 = 0;
         }
 
-
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            sBus.Text = (string) dataGridView1.SelectedCells[0].Value;
+        }
     }
  }
